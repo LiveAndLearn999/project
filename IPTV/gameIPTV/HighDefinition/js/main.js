f271f9a6fe8184933aeed4724009e1eaa9f0d6a9
徐克 <2295980314@qq.com>
2018-06-28 16:52:30 +0800

    IPTV

---
+++ IPTV/gameIPTV/HighDefinition/js/main.js
@@ -0,0 +1,142 @@
+		function fouc(d){
+			document.getElementById(d).style.background="url('../img/nav/hover.png') -2px 8px no-repeat";
+		}
+		function blur1(c){
+			console.log(1111);
+			document.getElementById(c).style.background="none";
+			
+		}
+		function fouc2(){
+			document.getElementById("individual").style.background="url('../img/nav/individualHover.png') 13px 18px no-repeat";
+		}
+		function blur2(){
+			document.getElementById("individual").style.background="none";
+		}
+		function fouc3(){
+			document.getElementById("vid").style.background="url('../img/playHover.png') 0px 0px no-repeat";
+			document.getElementById("vid").style.backgroundSize = "505px 292px";
+		}
+		function blur3(){
+			document.getElementById("vid").style.background= "none";
+		}
+		function fouc4(){
+			document.getElementById("banner1").style.background="url('../img/banner/bannerHover1.png') -10px -10px no-repeat";
+			document.getElementById("vid").style.backgroundSize = "275px 292px";
+		}
+		function blur4(){
+			document.getElementById("banner1").style.background="none";
+		}
+		function fouc5(d){
+			document.getElementById(d).style.background="url('../img/newAndhot/hover.png') -10px -10px no-repeat";
+		}
+		function blur5(d){
+			document.getElementById(d).style.background="none";
+			
+		}
+		function fouc6(d){
+			document.getElementById(d).style.background="url('../img/footer/hover.png') -10px -10px no-repeat";
+			document.getElementById("antiTerrorism").style.backgroundSize = "295px 212px";
+		}
+		function blur6(d){
+			document.getElementById(d).style.background="none";
+		}
+		function fouc7(d){
+			document.getElementById(d).style.background="url('../img/footer/hover.png') -5px 0px no-repeat";
+			document.getElementById(d).style.backgroundSize = "285px 220px";
+		}
+		function fouc8(d){
+			document.getElementById(d).style.background="url('../img/spanRecommend/hover.png') -8px -8px no-repeat";
+			// document.getElementById(d).style.backgroundSize = "285px 220px";
+		}
+		function fouc9(d){
+			document.getElementById(d).style.background="url('../img/spanRecommend/hover2.png') -8px -8px no-repeat";
+			// document.getElementById(d).style.backgroundSize = "285px 220px";
+		}
+		function fouc10(d){
+			document.getElementById(d).style.background="url('../img/banner/bannerHover2.png') -8px 0px no-repeat";
+			  document.getElementById(d).style.backgroundSize = "260px 300px";
+		}
+		function fonle(){
+			document.getElementById("lebtn").style.background="url('../img/Flippage/left2.png') 0px 0px no-repeat";
+		}
+		function fonrig(){
+			document.getElementById("rigbtn").style.background="url('../img/Flippage/right2.png') 0px 0px no-repeat";
+		}
+		function fouc11(){
+			console.log(1111);
+			document.getElementById("recentBrowse").style.background="url('../img/individual/recentBrowse/hover.png') -10px -9px no-repeat";
+		}
+		function fouc12(){
+			document.getElementById("myCollect").style.background="url('../img/individual/myCollect/hover.png') -10px -9px no-repeat";
+		}
+		function fouc12(){
+			document.getElementById("myCollect").style.background="url('../img/individual/myCollect/hover.png') -10px -9px no-repeat";
+		}
+		function fouc13(){
+			document.getElementById("individualRecommend").style.background="url('../img/individual/spanRecommend/hover.png') -10px -8px no-repeat";
+			document.getElementById("individualRecommend").style.backgroundSize = "230px 260px";
+		}
+		function fouc14(){
+			document.getElementById("allPlay").style.background="url('../img/individual/allPlay/hover.png') -9px -9px no-repeat";
+		}
+		function fouc15(){
+			document.getElementById("allDelete").style.background="url('../img/individual/allDelete/hover.png') -9px -9px no-repeat";
+		}
+		function fouc16(d){
+			document.getElementById(d).style.background="url('../img/individual/allDelete/hover.png') -9px -9px no-repeat";
+		}
+		function fouc17(d){
+			document.getElementById(d).style.background= "url('../img/individual/play/playBackground.png') -8px -10px no-repeat";
+		}
+		function fouc18(d){
+			document.getElementById(d).style.background="url('../img/individual/play/playActive.png') 13px 16px no-repeat";
+		}
+		function fouc19(d){
+			document.getElementById(d).style.background="url('../img/individual/play/heartActive.png') 11px 15px no-repeat";
+			document.getElementById(d).style.backgroundSize = "25px 21px";
+		}
+		function fouc20(d){
+			document.getElementById(d).style.background="url('../img/individual/play/garbageActive.png') 9px 16px no-repeat";
+			
+		}
+		function fouc21(d){
+			document.getElementById(d).style.background="none";
+			
+		}
+		function fouc22(d){
+			d.style.background = "url('../img/thirdPage/hover.png') -10px -8px no-repeat";
+			d.style.backgroundSize = "589px 65px"
+		}
+		function blur22(d){
+			d.style.background = "none";
+		}
+		function fouc23(d){
+			d.style.background = "url('../img/Flippage/righttop2.png') -11px 3px no-repeat";
+		}
+		function blur23(d){
+			d.style.background = "url('../img/Flippage/righttop1.png') -10px 3px no-repeat";
+		}
+		function fouc24(d){
+			d.style.background = "url('../img/Flippage/lefttop2.png') -11px 3px no-repeat";
+		}
+		function blur24(d){
+			d.style.background = "url('../img/Flippage/lefttop1.png') -10px 3px no-repeat";
+		}
+		function fouc25(){
+			document.getElementById('orderDay').style.background = "url('../img/order/orderDay.png') 0px -4px no-repeat";
+		}
+		function fouc26(){
+			document.getElementById('orderMonth').style.background = "url('../img/order/orderMoth.png') 0px -3px no-repeat";
+		}
+		function fouc27(){
+			document.getElementById('back').style.background = "url('../img/order/back.png') 0px -3px no-repeat";
+		}
+		function fouc28(){
+			document.getElementById('wait').style.background = "url('../img/popover/wait.png') -14px -5px no-repeat";
+		}
+		function fouc29(){
+			document.getElementById('reject').style.background = "url('../img/popover/reject.png') -15px -5px no-repeat";
+		}
+		function dd(){
+			console.log(33333);
+		}
---
+++ IPTV/gameIPTV/standardDefinition/js/main.js
@@ -0,0 +1,42 @@
+function fouc(d){
+			document.getElementById(d).style.background="url('../img/nav/hover.png') -5px 3px no-repeat";
+		}
+		function blur1(c){
+			console.log(1111);
+			document.getElementById(c).style.background="none";
+			
+		}
+		function fouc2(){
+			document.getElementById("individual").style.background="url('../img/nav/individualHover.png') 16px 14px no-repeat";
+		}
+		function blur2(){
+			document.getElementById("individual").style.background="none";
+		}
+		function fouc3(){
+			document.getElementById("vid").style.background="url('../img/playHover.png') 0px 0px no-repeat";
+			document.getElementById("vid").style.backgroundSize = "263px 217px";
+		}
+		function blur3(){
+			document.getElementById("vid").style.background= "none";
+		}
+		function fouc4(){
+			document.getElementById("banner1").style.background="url('../img/banner/bannerHover1.png') 12px -4px no-repeat";
+			document.getElementById("banner1").style.backgroundSize = "145px 224px";
+		}
+		function blur4(){
+			document.getElementById("banner1").style.background="none";
+		}
+		function fouc5(d){
+			document.getElementById(d).style.background="url('../img/newAndhot/hover.png') -4px -6px no-repeat";
+		}
+		function blur5(d){
+			document.getElementById(d).style.background="none";
+			
+		}
+		function fouc6(d){
+			document.getElementById(d).style.background="url('../img/footer/hover.png') -5px -5px no-repeat";
+			document.getElementById("antiTerrorism").style.backgroundSize = "149px 154px";
+		}
+		function blur6(d){
+			document.getElementById(d).style.background="none";
+		}
